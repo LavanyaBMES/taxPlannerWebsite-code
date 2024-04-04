3737865ac45dfbcd0ec459f8e8e0d74d5711a787
Lavanya <lavanyaj0725@gmail.com>
2024-04-04 18:03:41 +0530

    changes2

--- src/utils/context/index.jsx
+++ src/utils/context/index.jsx
@@ -1,4 +1,3 @@
-/*
 "use client";
 
 import { useState, createContext, useEffect } from "react";
@@ -36,28 +35,3 @@ const UserContextProvider = ({ children }) => {
 };
 
 export default UserContextProvider;
-*/
-"use client"
-import { createContext } from "react";
-export const UserContext = createContext();
-async function getData() {
-  const res = await fetch(
-    "https://services.taxplanner.co.in/paymentdetails-json.aspx"
-  );
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-}
-export default async function UserContextProvider({ children }) {
-  const data = await getData();
-  return (
-    <UserContext.Provider
-      value={{
-        data,
-      }}
-    >
-      {children}
-    </UserContext.Provider>
-  );
-}
